89daf2e5428ddcf97c483bfb522af5c3cbdcbb51
Bhuwanesh16 <bhuwaneshmanivel@gmail.com>
2025-05-25 22:42:42 +0530

    page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import './index.css';
+import { Routes, Route } from 'react-router-dom';
+import AuthLayout from "./components/auth/layout";
+import AuthLogin from "./pages/auth/login";
+import AuthRegister from "./pages/auth/register";
+import AdminLayout from './components/admin-view/layout';
+import AdminDashboard from './pages/admin-view/dashboard';
+import AdminProducts from './pages/admin-view/products';
+import AdminOrders from './pages/admin-view/orders';
+import AdminFeatures from './pages/admin-view/features';
 
 function App() {
 
   return (
-    <>
-      <div>
-        <h1 className='text-3xl font-bold underline'>Vite</h1>
+  
+      <div className="flex flex-col overflow-hidden bg-white">
+       <h1>Header Component</h1>
+       <Routes>
+        <Route path='/auth' element={<AuthLayout />}>
+          <Route path='login' element={<AuthLogin />} />
+          <Route path='register' element={<AuthRegister />} />
+        </Route>
+        <Route path="/admin" element={<AdminLayout/>}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="features" element={<AdminFeatures />} />
+        </Route>
+       </Routes>
       </div>
       
-    </>
+
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './store/store.js';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+    </Provider>
+  </BrowserRouter>
+
+
 )
